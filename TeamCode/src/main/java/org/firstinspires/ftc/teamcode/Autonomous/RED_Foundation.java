9f57551ca2f84f9c0608f6c89a4a9708bbbf6e96
Salthin <hverrill@protonmail.com>
2020-02-21 10:20:03 -0500

    RED_Foundation is working correctly. Rotate method speed issue resolved.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RED_Foundation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RED_Foundation.java
@@ -1,198 +1,73 @@
 //Imports:
 package org.firstinspires.ftc.teamcode.Autonomous;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.teamcode.FPS.Drivetrain;
-import org.firstinspires.ftc.teamcode.FPS.Measurement;
-import org.firstinspires.ftc.teamcode.FPS.Vision;
-
 
 /**
  * This program is Checkmate Robotics' Autonomous Program Template.
  */
 
-@Autonomous(name="RED_FOUNDATION", group="Tourny")
+@Autonomous(name="RED_Foundation", group="State")
 //@Disabled
 public class RED_Foundation extends LinearOpMode {
 
     /*
      *  Declare OpMode Members: */
     Drivetrain robot = new Drivetrain();
-    private VuforiaLocalizer vuforia = null;
 
-    public BNO055IMU revIMU;
     private ElapsedTime runtime = new ElapsedTime();
-    Vision portal = new Vision();
-    Measurement sensorSuite = null;
 
-    double margin = .5;
 
     @Override
     public void runOpMode() {
         robot.map(hardwareMap);
-        //robot = new Movement(this);
-        sensorSuite = new Measurement(revIMU, hardwareMap);
-        portal.createVuforia(VuforiaLocalizer.CameraDirection.BACK, hardwareMap, telemetry);
 
         waitForStart(); /** START THE PROGRAM */
+        runtime.reset();
 
 
-        strafe(.5);
-        sleep(500);
-        reverse(.35);
-        sleep(850);
-        stopAfter(0);
-        grabbersDown();
+        robot.leftHook.setPosition(.4);
+        robot.rightHook.setPosition(.6); // up
+        sleep(1200);
 
+        robot.reverse(5000, telemetry);
+        sleep(300);
 
+        robot.strafeLeft(9500, telemetry);//, telemetry
+        sleep(300);
 
+        robot.reverse(4500);
+        sleep(300);
 
-        sleep(2500);
+        robot.leftHook.setPosition(1);
+        robot.rightHook.setPosition(0); //grab
+        sleep(1200);
 
-//        stopAfter(500);
-//        robot.blockGrab.setPosition(0);
-//        setFourbarPos(1);
+        robot.forward(6700); // pull foundation
+        sleep(300);
 
-        forward(.5);
-        sleep(400);
-        stopAfter(0);
+        robot.rotate(-90, telemetry);
+        sleep(300);
 
-        rotate(-90);
-        reverse(.3);//push foundation into wall
-        sleep(400);
-        robot.leftHook.setPosition(.9);
-        robot.rightHook.setPosition(.1);
-        sleep(1500);
-        stopAfter(0);
+        robot.reverse(3000);
+        sleep(1000);
 
+        robot.leftHook.setPosition(.4);
+        robot.rightHook.setPosition(.6); // let go
+        sleep(1200);
 
+        robot.strafeRight(1300, telemetry);
+        sleep(300);
 
-        strafe(.2);
-        sleep(150);
-        stopAfter(0);
-        forward(.6);
-        sleep(750);
-        stopAfter(0);
+        robot.forward(13000, telemetry);
+        sleep(300);
 
-
-        while (!isStopRequested()) {
-            telemetry.addData("Angle 1 =", sensorSuite.getAngle().angle1);
-            telemetry.addData("Angle 2 =", sensorSuite.getAngle().angle2);
-            telemetry.addData("Angle 3 =", sensorSuite.getAngle().angle3);
-            telemetry.update();
-        }
+        sleep(999999);
 
     }
 
-
-    public void rotate(float degrees){
-
-        boolean turning = true;
-
-        float targetAngle = sensorSuite.getAngle().angle1 + degrees;
-        double ratio;
-        double powerPolarity = degrees/Math.abs(degrees);
-        double powerMultiplier;
-
-        while(turning && !isStopRequested()){
-
-            ratio = sensorSuite.getAngle().angle1/targetAngle;
-
-            powerMultiplier = 1-ratio;
-
-            if(Math.abs(powerMultiplier) < .15){
-                powerMultiplier = .2;
-            }
-            if(Math.abs(powerMultiplier) > .85){
-                powerMultiplier = .85;
-            }
-
-            robot.leftBack.setPower(-.2 * powerMultiplier * powerPolarity);
-            robot.leftFront.setPower(-.2 * powerMultiplier * powerPolarity);
-            robot.rightBack.setPower(.2 * powerMultiplier * powerPolarity);
-            robot.rightFront.setPower(.2 * powerMultiplier * powerPolarity);
-
-            if(ratio > .95) turning = false;
-
-        }
-        robot.leftBack.setPower(0);
-        robot.leftFront.setPower(0);
-        robot.rightBack.setPower(0);
-        robot.rightFront.setPower(0);
-    }
-    public void setFourbarPos(double pos){
-        robot.fourbarRight.setPosition(pos);
-        robot.fourbarLeft.setPosition(1-pos);
-    }
-    public void grabbersDown(){
-        robot.leftHook.setPosition(.27);
-        robot.rightHook.setPosition(.73);
-    }
-    public void stopAfter(long millis){
-        sleep(millis);
-        robot.leftFront.setPower(0);
-        robot.leftBack.setPower(0);
-        robot.rightFront.setPower(0);
-        robot.rightBack.setPower(0);
-        robot.intakeLeft.setPower(0);
-        robot.intakeRight.setPower(0);
-    }
-    public void turnClockwise(double power){// , long millis
-        robot.leftFront.setPower(-power);
-        robot.leftBack.setPower(-power);
-        robot.rightFront.setPower(power);
-        robot.rightBack.setPower(power);
-        //stopAfter(millis);
-    }
-    public void turnAntiClockwise(double power, long millis){
-        robot.leftFront.setPower(power);
-        robot.leftBack.setPower(power);
-        robot.rightFront.setPower(-power);
-        robot.rightBack.setPower(-power);
-        stopAfter(millis);
-    }
-    public void forward(double power){ //, long millis
-        robot.leftFront.setPower(power);
-        robot.leftBack.setPower(power);
-        robot.rightFront.setPower(power);
-        robot.rightBack.setPower(power);
-        //stopAfter(millis);
-    }
-    public void reverse(double power){ //, long millis
-        robot.leftFront.setPower(-power);
-        robot.leftBack.setPower(-power);
-        robot.rightFront.setPower(-power);
-        robot.rightBack.setPower(-power);
-        //stopAfter(millis);
-    }
-    public void strafe(double power){//, long millis
-        robot.leftFront.setPower(-power);
-        robot.leftBack.setPower(power);
-        robot.rightFront.setPower(power);
-        robot.rightBack.setPower(-power);
-        //stopAfter(millis);
-    }
-    public void succ(ElapsedTime time){
-        double starttime = time.milliseconds();
-        robot.intakeRight.setPower(-.6);
-        robot.intakeLeft.setPower(.6);
-        robot.leftFront.setPower(.15);
-        robot.leftBack.setPower(.15);
-        robot.rightFront.setPower(.15);
-        robot.rightBack.setPower(.12);
-        // loop until we detect a block or x seconds have expired
-
-        while(robot.blockToggle.getValue() < 1) {
-
-        }
-
-        stopAfter(0);
-
-    }
 }
