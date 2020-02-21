911c2981bede14f652770c8bdfafa4a0432713ab
Salthin <hverrill@protonmail.com>
2020-02-21 10:20:03 -0500

    RED_Foundation is working correctly. Rotate method speed issue resolved.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BLUE_Foundation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BLUE_Foundation.java
@@ -1,43 +1,31 @@
 //Imports:
 package org.firstinspires.ftc.teamcode.Autonomous;
-
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.teamcode.FPS.Drivetrain;
-import org.firstinspires.ftc.teamcode.FPS.Measurement;
-import org.firstinspires.ftc.teamcode.FPS.Vision;
-
 
 /**
  * This program is Checkmate Robotics' Autonomous Program Template.
  */
 
-@Autonomous(name="BLUE_Foundation", group="Tourny")
+@Autonomous(name="BLUE_Foundation", group="State")
 //@Disabled
 public class BLUE_Foundation extends LinearOpMode {
 
     /*
      *  Declare OpMode Members: */
     Drivetrain robot = new Drivetrain();
-    //private VuforiaLocalizer vuforia = null;
+
     private ElapsedTime runtime = new ElapsedTime();
-    //Vision portal = new Vision();
 
-
-    double margin = .5;
 
     @Override
     public void runOpMode() {
         robot.map(hardwareMap);
-        //portal.createVuforia(VuforiaLocalizer.CameraDirection.BACK, hardwareMap, telemetry);
 
         waitForStart(); /** START THE PROGRAM */
         runtime.reset();
-//        robot.resetEncoders();
 
 
         robot.leftHook.setPosition(.4);
@@ -77,75 +65,6 @@ public class BLUE_Foundation extends LinearOpMode {
         robot.strafeLeft(1000, telemetry);
 //        sleep(3000);
 
-//        while (!isStopRequested()) {
-//            telemetry.addData("Angle 1 =", robot.sensorSuite.getAngle().angle1);
-//            telemetry.addData("Angle 2 =", robot.sensorSuite.getAngle().angle2);
-//            telemetry.addData("Angle 3 =", robot.sensorSuite.getAngle().angle3);
-//            telemetry.update();
-//        }
-
     }
 
-
-    public void rotate(float degrees){
-
-        boolean turning = true;
-
-        float targetAngle = robot.sensorSuite.getAngle().angle1 + degrees;
-        double ratio;
-        double powerPolarity = degrees/Math.abs(degrees);
-        double powerMultiplier;
-
-        while(turning && !isStopRequested()){
-
-            ratio = robot.sensorSuite.getAngle().angle1/targetAngle;
-
-            powerMultiplier = 1-ratio;
-
-            if(Math.abs(powerMultiplier) < .15){
-                powerMultiplier = .2;
-            }
-            if(Math.abs(powerMultiplier) > .85){
-                powerMultiplier = .85;
-            }
-
-            robot.leftBack.setPower(-.2 * powerMultiplier * powerPolarity);
-            robot.leftFront.setPower(-.2 * powerMultiplier * powerPolarity);
-            robot.rightBack.setPower(.2 * powerMultiplier * powerPolarity);
-            robot.rightFront.setPower(.2 * powerMultiplier * powerPolarity);
-
-            if(ratio > .95) turning = false;
-
-        }
-        robot.leftBack.setPower(0);
-        robot.leftFront.setPower(0);
-        robot.rightBack.setPower(0);
-        robot.rightFront.setPower(0);
-    }
-    public void setFourbarPos(double pos){
-        robot.fourbarRight.setPosition(pos);
-        robot.fourbarLeft.setPosition(1-pos);
-    }
-    public void grabbersDown(){
-        robot.leftHook.setPosition(.4);
-        robot.rightHook.setPosition(.6);
-    }
-
-    public void succ(ElapsedTime time){
-        double starttime = time.milliseconds();
-        robot.intakeRight.setPower(-.6);
-        robot.intakeLeft.setPower(.6);
-        robot.leftFront.setPower(.15);
-        robot.leftBack.setPower(.15);
-        robot.rightFront.setPower(.15);
-        robot.rightBack.setPower(.12);
-        // loop until we detect a block or x seconds have expired
-
-        while(robot.blockToggle.getValue() < 1) {
-
-        }
-
-        robot.setPowerAll(0);
-
-    }
 }
